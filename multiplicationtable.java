4bb66760af7ec51a80c34b93b9db7ecbf155cbf0
muhangiarone <117397455+muhangiarone@users.noreply.github.com>
2022-11-03 23:23:47 +0300

    Add files via upload

---
+++ multiplicationtable.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.arone;
+
+/**
+ *
+ * @author Admin
+ */
+public class multiplicationtable {
+     public static void main(String[] args) {
+
+        int num = 6;
+        for(int i = 1; i <= 15; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+}
